ab9d9b14815787bd99604688ae6d5693d197938e
saiyaman <capsulecorplab@gmail.com>
2018-02-08 18:09:10 -0800

    updated readme and changed diagram class names from acronyms to full names in /sysml/diagrams.py

--- README.rst
+++ README.rst
@@ -4,6 +4,8 @@
 
 A Python package for SysML (systems modeling language) - a general purpose modeling language that supports analysis, design, and verification of complex systems.
 
+The design intent of this project is to serve as an interoperable and extensible object-oriented programming (OOP) toolkit, using the SysML (currently, v1.4) specification as the core modeling framework, for enabling model-based systems engineering.
+
 Notes
 --------
 This project is still in pre-alpha. For more details, please refer to https://github.com/spacedecentral/sysmlpy/issues
--- sysmlpy/diagrams.py
+++ sysmlpy/diagrams.py
@@ -9,15 +9,15 @@ SysML includes 9 types of diagrams, some of which are taken from UML.
 -Internal block diagram
 -Package diagram
 -Use case diagram
--Requirements diagram
+-Requirement diagram
 -Activity diagram
 -Sequence diagram
--State machine diagram
+-State diagram
 -Parametric diagram
 """
 
 ## Structure
-class bdd(object):
+class BlockDefinitionDiagram(object):
     """This class defines a block definition diagram
     
     A block definition diagram describes the system hierarchy and system/component classifications. 
@@ -26,7 +26,7 @@ class bdd(object):
     def __init__(self):
         pass
 
-class ibd(object):
+class InternalBlockDiagram(object):
     """This class defines an internal block diagram
     
     The internal block diagram describes the internal structure of a system in terms of its parts, ports, and connectors.
@@ -35,70 +35,69 @@ class ibd(object):
     def __init__(self):
         pass
 
-class pkg(object):
+class PackageDiagram(object):
     """This class defines a package diagram
-    
+
     The package diagram is used to organize the model.
     """
-    
+
     def __init__(self):
         pass
 
 ## Behavior
-class sd(object):
+class SequenceDiagram(object):
     """This class defines a sequence diagram
-    
-    A sequence diagram represents the interaction between collaborating parts of a system. 
+
+    A sequence diagram represents the interaction between collaborating parts of a system.
     """
-    
+
     def __init__(self):
         pass
 
-class stm(object):
+class StateDiagram(object):
     """This class defines a state machine diagram
-    
+
     The state machine diagram describes the state transitions and actions that a system or its parts perform in response to events.
      """
-    
+
     def __init__(self):
         pass
 
-class act(object):
+class ActivityDiagram(object):
     """This class defines an activity diagram
-    
+
     The activity diagram represents the flow of data and control between activities.
     """
-    
+
     def __init__(self):
         pass
 
-class uc(object):
+class UseCaseDiagram(object):
     """This class defines a use case diagram
-    
+
     A use-case diagram provides a high-level description of functionality that is achieved through interaction among systems or system parts.
     """
-    
+
     def __init__(self):
         pass
 
 ## Requirements
-class req(object):
-    """This class defines a requirements diagram
-    
-    The requirements diagram captures requirements hierarchies and requirements derivation, and the satisfy and verify relationships allow a modeler to relate a requirement to a model element that satisfies or verifies the requirements. 
+class RequirementDiagram(object):
+    """This class defines a requirement diagram
+
+    The requirements diagram captures requirements hierarchies and requirements derivation, and the satisfy and verify relationships allow a modeler to relate a requirement to a model element that satisfies or verifies the requirements.
     """
-    
+
     def __init__(self):
         pass
 
 ## Parametrics
-class par(object):
+class ParametricDiagram(object):
     """This class defines a parametric diagram
-    
+
     The parametric diagram represents constraints on system property values such as performance, reliability, and mass properties, and serves as a means to integrate the specification and design models with engineering analysis models.
     """
-    
+
     def __init__(self):
         pass
-
 
