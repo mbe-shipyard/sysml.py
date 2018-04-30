2797a531fa5027c15dcdbe2f3bba786be23582c5
saiyaman <capsulecorplab@gmail.com>
2018-04-30 16:55:47 -0700

    added skeleton tests for import_yaml

--- tests/test_import_yaml.py
+++ tests/test_import_yaml.py
@@ -8,26 +8,26 @@ def test_import_yaml():
     return model
 
 def test_elements(model):
-    "Display model elements (as a list of key value pairs?)"
+    "Display model elements (as a list of key-value pairs?)"
     model.elements
     #assert repr(model.elements) ==
 
-def test_elements(model):
-    "Display model relationships (as a list of key value pairs?)"
+def test_relationships(model):
+    "Display model relationships (as a list of key-value pairs?)"
     model.relationships
     #assert repr(model.relationships) ==
 
 def test_bdd(model):
-    "Generate a BlockDefinitionDiagram by specifying package label"
+    "Generate a Block Definition Diagram by specifying package label"
     bdd = model.bdd('FireSat Mission Requirements') # pass in package label for the BlockDefinitionDiagram to be generated
     #assert repr(bdd) ==
 
 def test_req(model):
-    "Generate RequirementDiagram by specifying package label"
+    "Generate Requirement Diagram by specifying package label"
     req = model.req('FireSat Mission Requirements') # pass in package label for the BlockDefinitionDiagram to be generated
     #assert repr(req) ==
 
-def test_par(model):
-    "Generate a BlockDefinitionDiagram by specifying package label"
-    bdd = model.bdd('FireSat Mission Requirements') # pass in package label for the BlockDefinitionDiagram to be generated
+def test_pkg(model):
+    "Generate a Package Diagram by specifying package label"
+    bdd = model.pkg('FireSat Mission Requirements') # pass in package label for the BlockDefinitionDiagram to be generated
     #assert repr(bdd) ==
