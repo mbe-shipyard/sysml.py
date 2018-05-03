5dc2afe65d8781ec1dba9b75a55bbc4e40d31d28
saiyaman <capsulecorplab@gmail.com>
2018-05-02 17:38:03 -0700

    changed _elements and _relationships in 'Model' to dictionaries

--- sysml/model.py
+++ sysml/model.py
@@ -14,30 +14,34 @@ class Model(object):
     """This class defines a SysML model for subsuming stereotypes into model elements or relationships.
     """
 
-    _validStereotypes = {"block":Block, "requirement":Requirement, "constraint":Constraint, "package":Package}
+    _validElements = {"block":Block, "requirement":Requirement, "constraint":Constraint, "package":Package}
+    _validRelationships = {}
 
-    def __init__(self, label=None, elements=None, relationships=None):
+    def __init__(self, label=None, elements={}, relationships={}):
         # Model label
         self._label = label
-        # List of model elements
+        # All model elements stored as a dictionary of key-value pairs
         self._elements = elements
-        # Relationship between model elements
+        # All model relationships stored as a dictionary of key-value pairs
         self._relationships = relationships
-        # All model elements and model relationships are stored as key-value pairs, where keys are generated upon assimilation
-        self._collective = {}
 
-    def __setitem__(self, idKey, item, overwrite=True):
-        "Adds stereotype-valid model element or relationship into model"
-        stereotype, id_no = idKey.split('-')
-        if stereotype in Model._validStereotypes.keys() and isinstance(int(id_no),int):
-            stereotypeInstance = Model._validStereotypes[stereotype](label=item.label)
-            self._collective[idKey] = stereotypeInstance
+    def __setitem__(self, key, item):
+        "Sets/overwrites stereotype-valid model element or relationship into model"
+        if not self._isValidKey(key):
+            raise ValueError(key + " is not a valid key. Keys should be a string containing a dash-separated stereotype and integer, e.g., 'block-42' ")
+        if not self._isValidStereotype(item):
+            raise TypeError(item + " is not a valid stereotype.")
         else:
-            raise ValueError(idKey + " is not a valid key. Keys should be a string containing a dash-separated stereotype and integer, e.g., 'block-42' ")
+            self._elements[key] = item
 
-    def __getitem__(self, idKey):
+    def __getitem__(self, key):
         "Returns data for key-specified model element or relationship"
-        return self._collective[idKey]
+        if key in self._elements.keys():
+            return self._elements[key]
+        elif key in self._relationships.keys():
+            return self._relationships[key]
+        else:
+            raise ValueError(key + " is not a valid key. Keys should be a string containing a dash-separated stereotype and integer, e.g., 'block-42' ")
 
     @property
     def elements(self):
@@ -46,28 +50,42 @@ class Model(object):
 
     @elements.setter
     def elements(self, elements):
-        """Sets list of model elements.
+        """Sets/rewrites model elements for entire model by passing model elements as key-value pairs.
 
         Note: model elements must be valid stereotypes.
         """
-        for element in elements:
-            if type(element) not in Model._validStereotypes.values():
-                raise TypeError(element + " is not a valid stereotype.")
-        self._elements = elements
+        if not type(elements) is dict:
+            raise TypeError(elements + " must be a dictionary.")
+        else:
+            for key in elements.keys():
+                if not self._isValidKey(key):
+                    raise ValueError(key + " is not a valid key. Keys should be a string containing a dash-separated stereotype and integer, e.g., 'block-42' ")
+                if not self._isValidStereotype(elements[key]):
+                    raise TypeError(elements[key] + " is not a valid stereotype.")
+            self._elements = elements
 
     @property
     def relationships(self):
         "Returns dictionary of relationships"
         return self._relationships
 
-    @relationships.setter
-    def relationships(self, relationships):
-        """Sets relationships to user-defined dictionary.
+    # @relationships.setter
+    # def relationships(self, relationships):
+    #     """Sets relationships to user-defined dictionary.
+    #
+    #     Note: model relationships must be valid stereotypes.
+    #     """
+    #     for key in relationships:
+    #         stereotype, id_no = key.split('-')
+    #         if stereotype not in Model._validElements:
+    #             raise TypeError(stereotype + "is not a valid stereotype.")
+    #     self._relationships[key] = relationships[key]
 
-        Note: model relationships must be valid stereotypes.
-        """
-        for key in relationships:
-            stereotype, id_no = key.split('-')
-            if stereotype not in Model._validStereotypes:
-                raise TypeError(stereotype + "is not a valid stereotype.")
-        self._relationships[key] = relationships[key]
+    @classmethod
+    def _isValidKey(cls, key):
+        stereotypeName, id_no = key.split('-')
+        return stereotypeName in cls._validElements.keys() or stereotypeName in cls._validRelationships.keys() and isinstance(int(id_no),int)
+
+    @classmethod
+    def _isValidStereotype(cls, item):
+        return type(item) in cls._validElements.values() or type(item) in cls._validRelationships.values()
--- tests/test_model.py
+++ tests/test_model.py
@@ -7,29 +7,41 @@ def model():
     model = sysml.Model('USS Enterprise')
     return model
 
-def test_elements(model):
-    "Set and get model elements as a list of valid stereotypes"
-    enterprise = sysml.Block('NCC-1701')
-    saucersection = sysml.Block('Primary hull')
-    model.elements = [enterprise, saucersection, sysml.Block('Secondary hull')]
-    assert repr(enterprise) == "\xabblock\xbb 'NCC-1701'"
-    assert repr(saucersection) == "\xabblock\xbb 'Primary hull'"
-    assert repr(model.elements) == "[\xabblock\xbb 'NCC-1701', \xabblock\xbb 'Primary hull', \xabblock\xbb 'Secondary hull']"
-
 def test_set_and_get_item(model):
-    "Model elements should be assigned a unique id, and accessible as key-value pair, upon assimilation"
+    "Set and get model elements or relationships using valid key"
     enterprise = sysml.Block('NCC-1701')
     saucersection = sysml.Block('Primary hull')
     model["block-1"] = enterprise
     model["block-2"] = saucersection
     model["block-3"] = sysml.Block('Secondary hull')
-    print(sysml.Model._validStereotypes.keys())
     assert repr(model["block-1"]) == "\xabblock\xbb 'NCC-1701'"
     assert repr(model["block-2"]) == "\xabblock\xbb 'Primary hull'"
     assert repr(model["block-3"]) == "\xabblock\xbb 'Secondary hull'"
+    with pytest.raises(TypeError) as e_info:
+        model["block-1"] = {"block-1": str('Alien Blob')}
     with pytest.raises(ValueError) as e_info:
         model["blob-1"] = sysml.Block('Alien Blob')
     # assert repr(e_info) == "blob-1 is not a valid key. Keys should be a string containing a dash-separated stereotype and integer, e.g., 'block-42' "
+
+def test_elements(model):
+    "Set and get model elements using valid key"
+    enterprise = sysml.Block('NCC-1701')
+    saucersection = sysml.Block('Primary hull')
+    model.elements = {"block-1":enterprise, 'block-2':saucersection, 'block-3':sysml.Block('Secondary hull')}
+    assert repr(enterprise) == "\xabblock\xbb 'NCC-1701'"
+    assert repr(saucersection) == "\xabblock\xbb 'Primary hull'"
+    assert repr(model.elements['block-1']) == "\xabblock\xbb 'NCC-1701'"
+    assert repr(model.elements['block-2']) == "\xabblock\xbb 'Primary hull'"
+    assert repr(model.elements['block-3']) == "\xabblock\xbb 'Secondary hull'"
+    with pytest.raises(TypeError) as e_info:
+        model.elements = {"block-1": str('Alien Blob')}
+    with pytest.raises(ValueError) as e_info:
+        model.elements = {"blob-1": sysml.Block('Alien Blob')}
+
+# def test_relationships(model):
+#     "Define relationships between model elements as source-target pairs"
+#     model.add_relationship(source = model.block['NCC-1701'], target = [saucersection, model.block['Secondary hull']], stereotype='composition')
+#     return model
 
 # @pytest.fixture
 # def add_blocks(model):
@@ -42,12 +54,6 @@ def test_set_and_get_item(model):
 #
 # def test_add_blocks(add_blocks):
 #     assert repr(model.elements) == "[\xabblock\xbb 'NCC-1701', \xabblock\xbb 'Primary hull', \xabblock\xbb 'Secondary hull']"
-#
-# @pytest.fixture
-# def create_relationships(add_blocks):
-#     "Define relationships between model elements as a list of edges"
-#     model.add_relationship(source = model.block['NCC-1701'], target = [saucersection, model.block['Secondary hull']], stereotype='composition')
-#     return model
 
 """
 def test_block_parts(add_block_relationships):
