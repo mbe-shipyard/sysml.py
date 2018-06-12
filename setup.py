df0525f34a839930bfba286ba5e825d37823fd59
Sean Marquez <capsulecorplab@gmail.com>
2018-06-12 16:28:20 -0700

    Changed version to '0.0.1', i.e., "alpha release"

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     #...,
     name='sysml',
-    version='0.1',
+    version='0.0.1',
     packages=['sysml'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
